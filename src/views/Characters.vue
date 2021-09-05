a732269f5c511f40c9f992b88f57ac78c6fb6bab
mourad5000 <mouradmounim7@gmail.com>
2021-09-05 13:07:29 +0200

    Add multisort to Race

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -6,8 +6,15 @@
     :items-per-page="20"
     :loading="loader"
     loading-text="Loading... Please wait"
-    class="elevation-1"
-  ></v-data-table>
+    mobile="true"
+    multi-sort
+    :sort-by="['race']"
+    :sort-desc="[false, true]"
+  >
+    <template v-slot:top>
+      <h1>The Lord of the Rings Characters</h1>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -15,17 +22,18 @@ export default {
   name: "Characters",
   data() {
     return {
+      singleExpand: false,
       headers: [
-        { text: "Name", value: "name", align: "start", sortable: false },
-        { text: "Birth", value: "birth", align: "start", sortable: false },
-        { text: "Death", value: "death", align: "start", sortable: false },
-        { text: "Gender", value: "gender", align: "start", sortable: false },
-        { text: "Hair", value: "hair", align: "start", sortable: false },
-        { text: "Height", value: "height", align: "start", sortable: false },
-        { text: "Race", value: "race", align: "start", sortable: false },
-        { text: "Realm", value: "realm", align: "start", sortable: false },
-        { text: "Spouse", value: "spouse", align: "start", sortable: false },
-        { text: "WikiUrl", value: "wikiUrl", align: "start", sortable: false }
+        { text: "Name", value: "name", align: "start" },
+        { text: "Birth", value: "birth", align: "start" },
+        { text: "Death", value: "death", align: "start" },
+        { text: "Gender", value: "gender", align: "start" },
+        { text: "Hair", value: "hair", align: "start" },
+        { text: "Height", value: "height", align: "start" },
+        { text: "Race", value: "race", align: "start" },
+        { text: "Realm", value: "realm", align: "start" },
+        { text: "Spouse", value: "spouse", align: "start" },
+        { text: "WikiUrl", value: "wikiUrl", align: "start" }
       ]
     };
   },
@@ -48,8 +56,3 @@ export default {
   }
 };
 </script>
-<style>
-.elevation-1 {
-  padding: 30px;
-}
-</style>
